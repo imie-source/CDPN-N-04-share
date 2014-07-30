4b0f5825594ccb80753414b3f69f569a51d12959
Simon Louvet <simon.louvet@imie.fr>
2014-07-30 17:09:23 +0200

    TP12

---
+++ JDBC/TP12/TPJDBC_CDPN03/src/org/imie/DAO/IPersonneDAO.java
@@ -0,0 +1,9 @@
+package org.imie.DAO;
+
+import org.imie.DTO.PersonneDTO;
+
+public interface IPersonneDAO extends IDAO<PersonneDTO> {
+
+
+
+}
---
+++ JDBC/TP12/TPJDBC_CDPN03/src/org/imie/exception/ImieException.java
@@ -0,0 +1,22 @@
+package org.imie.exception;
+
+public class ImieException extends Exception {
+
+	public ImieException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ImieException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ImieException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
